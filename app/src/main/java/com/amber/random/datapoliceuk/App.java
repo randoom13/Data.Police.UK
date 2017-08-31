f5fd02b8b050998769ba138843290958f961c29b
randoom13 <khlivnjuk@mail.ru>
2017-08-31 22:42:05 +0300

    prepared to android tests

--- app/src/main/java/com/amber/random/datapoliceuk/App.java
+++ app/src/main/java/com/amber/random/datapoliceuk/App.java
@@ -1,6 +1,7 @@
 package com.amber.random.datapoliceuk;
 
 import android.app.Application;
+import android.support.annotation.VisibleForTesting;
 
 import com.amber.random.datapoliceuk.di.component.AppComponent;
 import com.amber.random.datapoliceuk.di.component.DaggerAppComponent;
@@ -9,19 +10,24 @@ import com.amber.random.datapoliceuk.di.module.RestApiModule;
 import com.amber.random.datapoliceuk.di.module.ViewModelModule;
 
 public class App extends Application {
-    private static AppComponent mComponent;
+    private static AppComponent sComponent;
+
+    public static AppComponent getComponent() {
+        return sComponent;
+    }
+
+    @VisibleForTesting(otherwise = VisibleForTesting.NONE)
+    public static void setComponent(AppComponent component) {
+        sComponent = component;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-      mComponent = DaggerAppComponent.builder()
-              .networkModule(new NetworkModule())
-              .restApiModule(new RestApiModule())
-              .viewModelModule(new ViewModelModule())
-              .build();
-    }
-
-    public static AppComponent getComponent() {
-        return mComponent;
+        sComponent = DaggerAppComponent.builder()
+                .networkModule(new NetworkModule())
+                .restApiModule(new RestApiModule())
+                .viewModelModule(new ViewModelModule())
+                .build();
     }
 }
